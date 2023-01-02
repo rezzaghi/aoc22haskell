662fbb0ea2b71241ba0e64c912728f5d3e435633
rezzaghi <lbrezzaghi@gmail.com>
2023-01-02 13:39:02 -0300

    aoc 1day second challenge

--- 1/1.hs
+++ 1/1.hs
@@ -1,11 +1,12 @@
 import System.IO
 import Data.List.Split
+import Data.Sort
   
 main :: IO ()
 main = do     
     line <- getContents
     let list = formatInput line
-    print $ getMax (listOfStringToListOfInt list)
+    print $ getTop3Max (listOfStringToListOfInt list)
 
 formatInput :: String -> [[String]]
 formatInput input = map words (splitOn "\n\n" input)
@@ -13,6 +14,13 @@ formatInput input = map words (splitOn "\n\n" input)
 listOfStringToListOfInt :: [[String]] -> [[Int]]
 listOfStringToListOfInt list =  map (map (read::String->Int)) list
 
-getMax :: [[Int]] -> Int
-getMax list = maximum (map sum list)
+getTop3Max :: [[Int]] -> Int
+getTop3Max list = sum $ take 3 (sortList (sumListOfInts list))
+
+sortList :: [Int] -> [Int]
+sortList list = reverse (sort list)
+
+sumListOfInts :: [[Int]] -> [Int]
+sumListOfInts ints = map sum ints
+
 
